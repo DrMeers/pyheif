0327aa79967392829b5427e38da9a1a4907a42aa
Simon Meers <simon@simonmeers.com>
2023-03-15 08:54:46 +1100

    see if we can use our own "libheif/heif.h"?

--- libheif/libheif_build.py
+++ libheif/libheif_build.py
@@ -7,7 +7,7 @@ ffibuilder = FFI()
 with open("libheif/heif.h") as f:
     ffibuilder.cdef(f.read())
 
-include_dirs = ["/usr/local/include", "/usr/include", "/opt/local/include"]
+include_dirs = [".", "/usr/local/include", "/usr/include", "/opt/local/include"]
 library_dirs = ["/usr/local/lib", "/usr/lib", "/lib", "/opt/local/lib"]
 
 homebrew_prefix = os.getenv("HOMEBREW_PREFIX")
